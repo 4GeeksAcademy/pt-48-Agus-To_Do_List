2e3074808e3321727e1ac07ed6c9eb0b386b0a5c
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-11-08 22:00:01 +0000

    To Do List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [itemToDo, updateItemToDo] = useState("");
+  const [list, updateList] = useState({ itemToDo: "" });
+  const [listItems, updateListItems] = useState([]);
+  const [hoverStates, updateHoverStates] = useState([]);
+  const [height, updateHeight] = useState(20 + 'vh')
+
+  useEffect(() => {
+    updateList({ itemToDo: itemToDo });
+  }, [itemToDo]);
+
+  const toDoList = () => {
+    updateListItems([...listItems, list]);
+    updateItemToDo("");
+    updateHoverStates([...hoverStates, false]);
+    updateHeight(height + 5)
+  };
+
+  const handleMouseEnter = (index) => {
+    const newHoverStates = [...hoverStates];
+    newHoverStates[index] = true;
+    updateHoverStates(newHoverStates);
+  };
+
+  const handleMouseLeave = (index) => {
+    const newHoverStates = [...hoverStates];
+    newHoverStates[index] = false;
+    updateHoverStates(newHoverStates);
+  };
+
+  const handleDelete = (index) => {
+    const updatedList = [...listItems];
+    updatedList.splice(index, 1);
+    updateListItems(updatedList);
+    updateHeight(height - 5)
+  };
+
+  return (
+    <>
+      <div className="main__container text-center" style={{height: {height}}}>
+        <input
+          type="text"
+          onChange={(e) => updateItemToDo(e.target.value)}
+          value={itemToDo}
+          placeholder="What needs to be done?"
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              toDoList();
+            }
+          }}
+        />
+        <button
+          type="button"
+          className="btn btn-dark"
+          onClick={() => toDoList()}
+        >
+          Add
+        </button>
+        <ul className="items__container fa-ul mt-3">
+          {listItems.map((element, index) => (
+            <li key={index} className="mt-2">
+              <div
+                className="d-flex justify-content-between ms-2"
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={() => handleMouseLeave(index)}
+              >
+                {element.itemToDo}
+                {hoverStates[index] && (
+                  <button
+                    type="button"
+                    className="btn-close bg-secondary"
+                    aria-label="Close"
+                    onClick={() => handleDelete(index)}
+                  ></button>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+        <span>{listItems.length} things to do before the deadline!</span>
+      </div>
+    </>
+  );
+}
 
 export default Home;
